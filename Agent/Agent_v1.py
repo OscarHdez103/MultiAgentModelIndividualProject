ab1dfef3369eb8effdfa924704fffd1dcda7f61f
Oscar <123551720+OscarHdez103@users.noreply.github.com>
2026-03-03 14:22:05 +0000

    Agent_v1 post code

--- Agent/Agent_v1.py
+++ Agent/Agent_v1.py
@@ -1,6 +1,7 @@
 from numpy import random
 
 from Agent.BaseAgent import BaseAgent
+from Others.Post import Post
 
 
 class Agent_v1(BaseAgent):
@@ -9,13 +10,27 @@ class Agent_v1(BaseAgent):
         self.random_state = random.RandomState(seed=self.seed)
         # Generate opinions amount of random -1..1 values inclusive
         self.opinions :list[float] = (self.random_state.rand(opinions) *2-1).tolist()
-        self.activity :float = self.random_state.rand()
-        self.privacy :float = self.random_state.rand()
+        self.activity :float = self.random_state.rand() # Frequency of interacting of any kind
+        self.privacy :float = self.random_state.rand()  # Frequency of posting content
 
-    def post(self):
+    def post(self) -> Post:
         print(f"Agent {self.unique_id} posted with state {self.state}")
         ## TODO: Implement posting behaviour
-        pass
+        # 1. Get random number of opinions to be influenced by the post
+        min_tendencies, max_tendencies = 1, len(self.opinions)
+        num_tendencies = self.random_state.randint(min_tendencies, max_tendencies + 1)
+
+        # 2. Randomly select num_tendencies opinions to be influenced
+        selected_indices = self.random_state.choice(len(self.opinions), size=num_tendencies, replace=False)
+
+        # 3. For each selected opinion, generate a random influence tendency between -1 and 1, for the rest set it to None
+        opinion_tendency :list[float|None] = [
+            self.random_state.rand() * 2 - 1
+            if i in selected_indices else None
+            for i in range(len(self.opinions))
+        ]
+
+        return Post(self.unique_id, self.model, opinion_tendency)
 
     def state(self, state_id:str) -> float|list[float]:
         if state_id == "opinions":
